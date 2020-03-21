70611d7b5125bac4229431faf21a32eede408a75
foo <foo@bar.de>
2020-03-21 19:13:45 +0100

    fix: always add --no-sandbox

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import puppeteer from 'puppeteer'
-import isGitpod from 'is-gitpod'
 
 const oldLaunch = puppeteer.launch
 puppeteer.launch = async function (options = {}) {
   return oldLaunch.call(this, {
     ...options,
-    args: [...options.args || [], ...(await isGitpod()) ? ['--no-sandbox'] : []],
+    args: [...options.args || [], '--no-sandbox'],
   })
 }
 
