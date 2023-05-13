763469b03b68dae2d5199eced50849b71fd8c120
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-05-13 18:04:25 +0200

    fix: new headless mode

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import puppeteer from 'puppeteer'
 const oldLaunch = puppeteer.launch
 puppeteer.launch = function (options = {}) {
   return oldLaunch.call(this, {
+    headless: 'new',
     ...options,
     args: [...(options.args || []), '--no-sandbox'],
   })
